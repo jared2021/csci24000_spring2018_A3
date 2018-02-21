5e2fc1f522ad1ca6265b9d74c3e54e81a1e0e1d1
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-02-21 15:57:09 -0500

    Second commit added the paramaters to my new method but for some reason its saying it's not a part of my class when I have it defined in my .h file.

--- Player.cpp
+++ Player.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 Player::Player()
 {
-	char Horse[5];		
+			
 }
 Player::~Player()
 {
@@ -21,5 +21,57 @@ int Player::setShot()
 }
 int Player::getShot()
 {
-	return this->shot;
+	return this->shot;	
 }
+int Player::setScore()
+{
+	if(this->one==2&&this->two==1)
+	{
+		this->letter1=this->letter1++;
+		if (this->letter1==1)
+		{
+			this->Horse[0]='H';
+		}
+		else if(this->letter1==2)
+		{
+			this->Horse[1]='O';
+		}
+		else if (this->letter1==3)
+		{
+			this->Horse[2]='R';
+		}
+		else if (this->letter1==4)
+		{
+			this->Horse[3]='S';
+		}
+		else if (this->letter1==5)
+		{
+			this->Horse[4]='E'
+		}
+	}
+	else if (this->one==1&&this->two==2)
+	{
+		this->letter2=this->letter2++;
+		if (this->letter2==1)
+		{
+			this->Horse[0]='H';
+		}
+		else if (this->letter2==2)
+		{
+			this->Horse[1]='O';
+		}
+		else if (this->letter2==3)
+		{
+			this->Horse[2]='R';
+		}
+		else if (this->letter2==4)
+		{
+			this->Horse[3]='S';
+		}
+		else if (this->letter2==5)
+		{
+			this->Horse[4]='E';
+		}
+	}
+}
+
