df7c98fdf4f58be6a9af814113050382e5b59901
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-02-21 15:02:50 -0500

    First commit I have some stuff made for my Player object but I need to add more to it.

--- Player.h
+++ Player.h
@@ -4,3 +4,19 @@
 //received any help on this assignment.
 //
 //jascho
+#ifndef _PLAYER_H
+#define _Player_H
+
+class Player
+{
+	public:
+		Player();
+		~Player();
+		char Horse[5];
+		int setShot();
+		int getShot();
+	
+	private:
+		int shot;
+};
+#endif
