6e2a269e03ea11c8832a5b9e0973beb394ff70b8
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-02-21 15:04:04 -0500

    First commit I built some of the paramaters for my Player object but I need to add some more.

--- Player.cpp
+++ Player.cpp
@@ -4,3 +4,22 @@
 //received any help on this assignment.
 //
 //jascho
+#include "Player.h"
+#include <stdlib.h>
+#include <iostream>
+Player::Player()
+{
+	char Horse[5];		
+}
+Player::~Player()
+{
+
+}
+int Player::setShot()
+{
+	this->shot=rand()%2+1;
+}
+int Player::getShot()
+{
+	return this->shot;
+}
